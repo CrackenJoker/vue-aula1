7af1ebe3da3545b201017ea8466ddbd8bd94de49
CrackenJoker <joelsoncandomba85@gmail.com>
2024-06-19 11:12:11 +0100

    Aula1 conteudos basicos do vue

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>Meu outro Nome é {{ nome }}</p>
+    </div>
+</template>
+<script>
+export default{
+    name:'LifeCycle',
+    data(){
+        return{
+            nome:'Não sei ainda'
+        }
+    },
+    created(){
+        setTimeout(()=>{
+            this.nome = 'Carvalho'
+        },1000)
+    },
+    mounted(){
+        setTimeout(()=>{
+            this.nome = 'Candomba'
+        },2000)
+    }
+}
+</script>
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,21 @@
+<template>
+        <div>
+            <h1>Ola meu nome é {{ nome }} <LifeCycle/> </h1><br>
+            <h3>Trabalho como {{ profissao }}</h3>
+        </div>
+</template>
+<script>
+import LifeCycle from './LifeCycle.vue';
+export default{
+    name:'PrimeiroComponente',
+    components:{
+        LifeCycle
+    },
+    data() {
+        return {
+            nome:'Joelson',
+            profissao:'TI'
+        }
+    },
+}
+</script>
